0d71d584acfb53a5968529bfef601a224a936c51
U-ITX\i52738 <i52738@D2UA229211V.itx.ad>
2014-05-08 14:35:34 -0700

    Added highlighting of letter and abstracted search functionality

---
+++ Search.js
@@ -0,0 +1,73 @@
+(function() {
+
+    document.searchForm.search.focus();
+    //add search functionality
+    $('#searchBox').on('input', startSearch);
+
+    var searchTimeout;
+    var strictSearch = false;
+
+    function startSearch(e) {
+        if (searchTimeout !== undefined) clearTimeout(searchTimeout);
+        searchTimeout = setTimeout(searchField, 500);
+    }
+
+    function searchField() {
+        var searchVal = $('#searchBox').val();
+        searchVal = searchVal.replace(/ /g, '');
+        var charSearch = searchVal.split('');
+        for (var chars in charSearch) {
+            charSearch[chars] += ".*";
+        }
+        searchVal = charSearch.join('');
+        var regExp = new RegExp(searchVal, 'i');
+        $('#tabsList .title').each(function() {
+            if (regExp.test($(this).text())) {
+                $(this).parent().show();
+
+                var highlightedText =
+                    getFormattedSearchText($('#searchBox').val(), $(this).text());
+
+                $(this).text('');
+                $(this).append(highlightedText);
+            } else
+                $(this).parent().hide();
+        });
+        $('.highlighted').removeClass('highlighted');
+        $('#tabsList > li:visible:first').addClass('highlighted');
+    }
+
+
+    function getNonExactRegex(searchString) {
+
+        searchString = searchString.replace(/ /g, '');
+        var charSearch = searchString.split('');
+        for (var chars in charSearch) {
+            charSearch[chars] += ".*";
+        }
+        searchString = charSearch.join('');
+        var regExp = new RegExp(searchString, 'i');
+        return regExp;
+    }
+
+    function getFormattedSearchText(searchString, text) {
+        var textArray = text.split('');
+        var charSearchArray = searchString.replace(/ /g, '').split('');
+        var lastIndex = charSearchArray.length - 1;
+        var index = 0;
+
+        for (var i in textArray) {
+            if (index <= lastIndex) {
+                if (charSearchArray[index].toUpperCase() === textArray[i].toUpperCase()) {
+
+                    textArray[i] = '<strong style="color:red">' + textArray[i] + '</strong>';
+                    index++;
+                }
+            } else {
+                break;
+            }
+        }
+
+        return textArray.join('');
+    }
+})();
