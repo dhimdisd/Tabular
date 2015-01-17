1d7e09e8ac75a18062d62dc9b96199d7039aaf67
dhimdisd <dhimdisd@gmail.com>
2015-01-16 17:00:13 -0800

    make visual adjustments like making font smaller

--- src/js/background.js
+++ src/js/background.js
@@ -37,6 +37,10 @@
     });
   };
 
+  w.getFilteredTabs = function(){
+    return w.tabs.filter(w.notInternalTab);
+  };
+
   w.getTabs();
 
   function indexOfTab(tabId) {
@@ -49,6 +53,7 @@
   }
 
   chrome.tabs.onCreated.addListener(function(tab) {
+    //We should probably check if tab exist in the list
       w.tabs.unshift(tab);
   });
 
@@ -90,7 +95,7 @@
         w.popupWindowId = null;
       });
     }
-  });
+  }); 
 
   //Listens for Key to popup app
   chrome.commands.onCommand.addListener(function(command) {
